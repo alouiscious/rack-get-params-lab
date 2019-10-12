82931d694e1b5091d3da7c5a87efaa387bdf04c5
Alouiscious Media <info@reneesoul.com>
2019-10-11 23:51:59 -0700

    Done.

--- app/application.rb
+++ app/application.rb
@@ -16,19 +16,20 @@ class Application
     elsif req.path.match(/search/)
       search_term = req.params["q"]
       resp.write handle_search(search_term)
-    elsif req.path.match(/cart/) 
-      resp.write "Your cart is empty." if @@cart.empty?
-      resp.write @@cart.join("\n")
     elsif req.path.match(/add/)
       add_item = req.params["item"]
       if @@items.include? add_item
         @@cart << add_item
-        resp.write "#{add_item} was added to your cart."
+        resp.write "added #{add_item}"
       else
-        resp.write "Sorry, we don't have that item"
-      end
+        resp.write "We don't have that item"
+      end  
+    elsif req.path.match(/cart/) 
+      resp.write "Your cart is empty." if @@cart.empty?
+      resp.write @@cart.join("\n")
     elsif req.path == "/"
       resp.write "<h1>Welcome Home!!!</h1>"
+    
     else
       resp.write "Path Not Found"
     end
